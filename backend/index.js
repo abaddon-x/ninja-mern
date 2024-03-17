872da27945d4ca7495e3d55fd957cfa77d4a2d19
Aldi Alfiandi <81903763+abaddon-x@users.noreply.github.com>
2024-03-17 21:57:55 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env, () => {
-      console.log("connected to db & listening on port", process.env);
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
     });
   })
   .catch((error) => {
