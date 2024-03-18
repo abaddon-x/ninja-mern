0cbaf2b974073d71e5aa3ecf812b1dd14f93e0ba
Aldi Alfiandi <81903763+abaddon-x@users.noreply.github.com>
2024-03-18 16:47:43 +0700

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
   const { workouts, dispatch } = useWorkoutsContext();
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts");
+      const response = await fetch("https://ninja-mern-server.vercel.app/api/workouts");
       const json = await response.json();
 
       if (response.ok) {
