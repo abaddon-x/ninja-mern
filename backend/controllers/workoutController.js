137d95c35819b0cfd2789cd08b403cbb59046ee9
Aldi Alfiandi <81903763+abaddon-x@users.noreply.github.com>
2024-03-18 18:18:47 +0700

    Update workoutController.js

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -3,29 +3,35 @@ const mongoose = require("mongoose");
 
 // get all workouts
 const getWorkouts = async (req, res) => {
+  
   const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  
   res.status(200).json(workouts);
+  
 };
 
 // get single workout
 const getWorkout = async (req, res) => {
+  
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such workout" });
-  } else {
+  }
+  
     const workout = await Workout.findById(id);
 
     if (!workout) {
       return res.status(404).json({ error: "No such workout" });
-    } else {
-      return res.status(200).json(workout);
     }
-  }
+  
+      return res.status(200).json(workout);  
+  
 };
 
 // create new workout
 const createWorkout = async (req, res) => {
+  
   const { title, load, reps } = req.body;
 
   let emptyFields = [];
@@ -55,32 +61,37 @@ const createWorkout = async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
+  
 };
 
 // delete workout
 const deleteWorkout = async (req, res) => {
+  
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such workout" });
-  } else {
+  }
+  
     const workout = await Workout.findOneAndDelete({ _id: id });
 
     if (!workout) {
       return res.status(404).json({ error: "No such workout" });
-    } else {
+    } 
+  
       res.status(200).json(workout);
-    }
-  }
+    
 };
 
 // update workout
 const updateWorkout = async (req, res) => {
+  
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such workout" });
-  } else {
+  }
+  
     const workout = await Workout.findOneAndUpdate(
       { _id: id },
       {
@@ -90,10 +101,10 @@ const updateWorkout = async (req, res) => {
 
     if (!workout) {
       return res.status(404).json({ error: "No such workout" });
-    } else {
-      res.status(200).json(workout);
     }
-  }
+  
+      res.status(200).json(workout);
+  
 };
 
 module.exports = {
