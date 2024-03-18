a005d993ebdf43cea2b58355397072364a001dfc
uncover-id <uncover.all.86@gmail.com>
2024-03-18 17:03:28 +0700

    init

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use("https://ninja-mern-server.vercel.app/api/workouts", workoutRoutes);
+app.use("/api/workouts", workoutRoutes);
 
 // connect to db
 mongoose
